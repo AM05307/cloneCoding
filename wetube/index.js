28f1119fba78660727d1321bc0a94e68afb1997d
AM05307 <eth05307@gmail.com>
2020-01-27 23:38:45 +0900

    express.js import

--- wetube/index.js
+++ wetube/index.js
@@ -1,6 +1,9 @@
-//require : node module을 어딘가에서 가져옴
+//require : node module을 어딘가에서 가져옴 = express import 
+// 1순위 : express 폴더를 찾고 
+// 2순위 : node_modules 사이에서 찾음 
 const express = require('express')
 
+// app변수 안에 express를 실행해서 담음
 const app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
