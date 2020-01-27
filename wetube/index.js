52dedd9e87eed4a17dc1467f3cb70d4156c30ff5
AM05307 <eth05307@gmail.com>
2020-01-27 23:34:52 +0900

    express.js 시작

--- wetube/index.js
+++ wetube/index.js
@@ -1,1 +1,9 @@
-console.log("hi");
+//require : node module을 어딘가에서 가져옴
+const express = require('express')
+
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
