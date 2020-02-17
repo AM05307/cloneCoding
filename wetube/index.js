1f51013eb2af132814b2de654c27d86a94575f7a
AM05307 <eth05307@gmail.com>
2020-02-17 23:27:08 +0900

    feat : bodyParser 설정

--- wetube/index.js
+++ wetube/index.js
@@ -22,7 +22,9 @@ const handleHome = (req, res) => res.send("Hello from Home");
 const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
-app.use(bodyParser());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded ({ extended : true}));
 
 app.use(helmet());
 app.use(morgan("dev"));
