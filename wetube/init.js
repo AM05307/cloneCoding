c7e5cca77edbc6a430c1df365ff9b4c01bae0be0
AM05307 <eth05307@gmail.com>
2020-02-22 01:49:00 +0900

    feat : app import

--- wetube/init.js
+++ wetube/init.js
@@ -1,30 +1,5 @@
-//require : node module을 어딘가에서 가져옴 = express import 
-// 1순위 : express 폴더를 찾고 
-// 2순위 : node_modules 사이에서 찾음 
-//const express = require('express');
+import app from "app";
 
-import express from "express";
-import morgan from "morgan";
-import helmet from "helmet";
-import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
+const PORt = 4000; 
 
-// app변수 안에 express를 실행해서 담음
-const app = express();
-
-const handleHome = (req, res) => res.send("Hello from Home");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
-app.use(cookieParser());
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded ({ extended : true}));
-
-app.use(helmet());
-app.use(morgan("dev"));
-
-// 메인 url로 접속시 get 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-
+const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
