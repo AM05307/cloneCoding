e5339a4e8490912fe8caf2f6343016c79ce2c917
AM05307 <eth05307@gmail.com>
2020-02-22 03:13:42 +0900

    feat : glovalRouter 추가

--- wetube/app.js
+++ wetube/app.js
@@ -10,17 +10,18 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 // app변수 안에 express를 실행해서 담음
 const app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded ({ extended : true}));
+app.use(bodyParser.urlencoded ({extended : true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
 app.use("/video", videoRouter);
 
