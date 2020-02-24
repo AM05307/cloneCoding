836d938094f3dd53b37434650d5c1e21969e9895
AM05307 <eth05307@gmail.com>
2020-02-24 22:03:08 +0900

    bug : router - controller 버그 수정

--- wetube/controller/videoController.js
+++ wetube/controller/videoController.js
@@ -2,6 +2,7 @@ export const home = (req, res) =>
   res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => 
   res.render("search", { pageTitle: "Search" });
+export const videos = (req, res) => res.send("Videos");
 export const upload = (req, res) => 
   res.render("upload", { pageTitle: "Upload" });
 export const videoDetail = (req, res) => 
--- wetube/routers/videoRouter.js
+++ wetube/routers/videoRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
 import routes from "../routes";
-import { videos, upload, videoDetails, editVideo, deleteVideo } from "../controller/videoController";
-
+import { videos, upload, videoDetail, editVideo, deleteVideo } from "../controller/videoController";
+ 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetails);
+videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
