dca68d7023e632ff8ef57c14021cc97581480195
AM05307 <eth05307@gmail.com>
2020-02-24 21:17:03 +0900

    app.js:

--- wetube/app.js
+++ wetube/app.js
@@ -1,3 +1,4 @@
+  
 //require : node module을 어딘가에서 가져옴 = express import 
 // 1순위 : express 폴더를 찾고 
 // 2순위 : node_modules 사이에서 찾음 
@@ -8,23 +9,25 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
 // app변수 안에 express를 실행해서 담음
 const app = express();
 
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded ({extended : true}));
-app.use(helmet());
+
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 
 export default app;
-
