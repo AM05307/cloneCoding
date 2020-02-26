239b2a395ee4c938f15fc33dc15bff76bd76196a
AM05307 <eth05307@gmail.com>
2020-02-26 23:35:56 +0900

    feat : searchController

---
+++ wetube/controller/test.js
@@ -0,0 +1,45 @@
+// Global
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+
+
+// Courses
+const CONFIRM_ACCOUNT = "/confirm-account";
+const COURSES = "courses";
+const NEW = "/new";
+const MINE = "/mine";
+
+//api
+const API = "/api";
+const DOCUMENTATION = "/documentaion";
+
+// api - v1
+const V1 = "/v1";
+const BUY = "buy";
+const REFUND = "refund";
+
+// api - v2
+const V2 = "/v2";
+const REMOVE = "/remove";
+const EDIT = "/edit";
+
+const routes = {
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  confirmAccount: CONFIRM_ACCOUNT,
+  courses: COURSES,
+  new: NEW,
+  mine: MINE,
+  api: API,
+  documentation: DOCUMENTATION,
+  v1: V1,
+  buy: BUY,
+  refund: REFUND,
+  v2: V2,
+  remove: REMOVE,
+  edit: EDIT
+};
+
+export default routes;
