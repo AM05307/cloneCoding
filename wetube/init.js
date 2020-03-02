abb24c901d1d583337902a91283a84180259b489
AM05307 <eth05307@gmail.com>
2020-03-03 00:23:05 +0900

    feat : 3.4. Comment Model 생성

--- wetube/init.js
+++ wetube/init.js
@@ -2,8 +2,10 @@ import "./db";
 import app from "./app";
 import dotenv from "dotenv";
 
+
 dotenv.config();
 import "./models/Video";
+import "./models/Comment";
 
 const PORT = process.env.PORT || 4000;
 
