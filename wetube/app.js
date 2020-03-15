0bca466100b084b2fb5770e6af300c919ffe7ee1
AM05307 <eth05307@gmail.com>
2020-03-15 22:43:05 +0900

    6.4 Sessions on Express{

--- wetube/app.js
+++ wetube/app.js
@@ -4,6 +4,8 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import { localsMiddleware } from "./middlewares";
+import passport from "passport";
+import session from "express-session";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -18,6 +20,15 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.user(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: true,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
